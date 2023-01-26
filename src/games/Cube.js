409e0ea79f3b2deb034a49966a63b3a340c56478
Alok Dubey <alok73327@gmail.com>
2023-01-26 08:21:25 +0530

    cube game issue fixed

--- src/games/Cube.js
+++ src/games/Cube.js
@@ -6,8 +6,8 @@ export default function Cube() {
       <iframe
         style={{ width: "100%", height: "100vh", margin: 0, padding: 0 }}
         title="cube"
-        src="http://localhost:8000/cube"
-        // src="https://yaariscore.azurewebsites.net/cube"
+        // src="http://localhost:8000/cube"
+        src="https://yaariscore.azurewebsites.net/cube"
       />
     </div>
   );
